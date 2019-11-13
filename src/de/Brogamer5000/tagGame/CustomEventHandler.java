402d2b1fc9d806a9588f0969b4de086552dfafbf
F-H00 <47611528+F-H00@users.noreply.github.com>
2019-11-13 23:36:25 +0100

    Updated Files
    - Einzelne Verbesserungen am Code
    - Es wird nun das Worldguard "DisallowPvpEvent" genutzt, anstatt des EntityHitByEntity-Events
    - Anstatt die Tag-Liste und die Tabu-Liste bei jeder Veränderung direkt abzuspeichern, wird dies nun nur einmal beim Deaktivieren gemacht.
    - Ist beim Laden des Plugins die Tag-Liste leer, wird ein Platzhalter eingefügt mit dem namen "dummyPlayer - please use /taggame reset"
    
    Offene Probleme:
    - Wenn ein Spieler einen anderen tagged, bekommt er trotzdem die Nachricht "Hey! Sorry, but you can´t pvp here!"
    - Code bereinigung steht noch immer offen, weil ich sehr, sehr faul bin :P

--- src/de/Brogamer5000/tagGame/CustomEventHandler.java
+++ src/de/Brogamer5000/tagGame/CustomEventHandler.java
@@ -6,7 +6,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import com.sk89q.worldguard.bukkit.protection.events.DisallowedPVPEvent;
 
 import de.Brogamer5000.tagGame.util.tagStorage;
 
@@ -15,40 +18,49 @@ public class CustomEventHandler implements Listener{
 	
 	tagGame plugin;
 	
+	
+	
 	public CustomEventHandler (tagGame plugin) {
 		this.plugin = plugin;
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
 	
-    @EventHandler(priority = EventPriority.MONITOR)
-	public void onHit(EntityDamageByEntityEvent e) {
-    	System.out.println("entity hit");
-        if (e.getEntity() instanceof Player && e.getDamager() instanceof Player) {
-            Player target = (Player) e.getEntity();
-            Player attacker = (Player) e.getDamager();
+	
+	
+	
+    @EventHandler(priority = EventPriority.HIGHEST)
+	public void onHit(DisallowedPVPEvent e) {
+    	
+
+    	//get target and attacker from DisallowedPVPEvent
+    	Player target = (Player) e.getDefender();
+    	Player attacker = (Player) e.getAttacker();
+    	
+
+    	System.out.println("test");
+
+    	
+        if (target instanceof Player && attacker instanceof Player) {
             
             
-            //get Infos and methods of the tagged Player
-            tagStorage taggedPlayer = this.plugin.commandExecutor.taggedPlayer;
-            
-            
+            //get current infos and methods of the tagged Player
+            tagStorage taggedPlayer = tagGame.commandExecutor.taggedPlayer;
             List<String> tagHistory = taggedPlayer.historyList;
-            String currentTag = "unset";
-            String lastTag = "unset";
-            if(tagHistory.size() > 0) {
-            	currentTag = tagHistory.get(tagHistory.size()-1);
-            }
-            if(tagHistory.size() > 1) {
-            	lastTag = tagHistory.get(tagHistory.size()-2);	
-            }
             
+            String currentTag = 	(tagHistory.size() >= 1) ? tagHistory.get(tagHistory.size()-1) : "unset";
+            String lastTag = 		(tagHistory.size() >= 2) ? tagHistory.get(tagHistory.size()-2) : "unset";
+            
+            
+            
+            //test, if the attacker is the tagged player
             if(currentTag.equalsIgnoreCase(attacker.getName())) {
             	
+            	
+            	//test, if the target is currently marked as afk (not used)
             	boolean targetIsAfk = false;
             	
-            	
             	/*
-            	 * 
+            	 
                 //try to get Infos from the afk plugin
                 try {
                 	Plugin zw2afk = plugin.getServer().getPluginManager().getPlugin("Zw2Afk");
@@ -63,29 +75,39 @@ public class CustomEventHandler implements Listener{
                 
                 */
             	
-            	
-            	
-            	
-                
-                
+            	//test, if target is listed in the taboo list
             	if(taggedPlayer.isTaboo(target.getName())) {
-            		attacker.sendMessage(plugin.pluginPraefix + "§4" + target.getName() + " steht auf der Tabu Liste!");
+            		
+            		attacker.sendMessage(tagGame.prefix + "§4" + target.getName() + " steht auf der Tabu Liste!");
+            	
             	}
-            	else if(targetIsAfk) {
-            		attacker.sendMessage(plugin.pluginPraefix + "§4" + target.getName() + " ist Afk!");
-            	}
+            	
+            	//else: test, if the target was the one, that gave the tag to the attacker
             	else if(lastTag.equalsIgnoreCase(target.getName())) {
-            		attacker.sendMessage(plugin.pluginPraefix + "§4Du kannst " + target.getName() + " nicht zurück taggen!");
+            		
+            		attacker.sendMessage(tagGame.prefix + "§4Du kannst " + target.getName() + " nicht zurück taggen!");
+            	
             	}
+            	
+            	//else: test, if target is afk at the moment
+            	else if(targetIsAfk) {
+            	
+            		attacker.sendMessage(tagGame.prefix + "§4" + target.getName() + " ist Afk!");
+            	
+            	}
+            	
+            	//else: attacker is succesfully tagging the target!
             	else {
-            		attacker.sendMessage(plugin.pluginPraefix + "Du hast " + target.getName() + " erfolgreich getaggt!");
-            		target.sendMessage(plugin.pluginPraefix + "Du wurdest von " + attacker.getName() + " getaggt!");
-                	taggedPlayer.set(target.getName());            		
+            		
+            		//send Messages to both, the attacker and the target
+            		attacker.sendMessage(tagGame.prefix + "Du hast " + target.getName() + " erfolgreich getaggt!");
+            		target.sendMessage(tagGame.prefix + "Du wurdest von " + attacker.getName() + " getaggt!");
+                	
+            		//call the set function in the instance of taggedPlayer
+            		taggedPlayer.set(target.getName());            		
+                    
             	}
-            }
-            
-            
-        }
+            }   
+        }        
     }
-	
 }
--- src/de/Brogamer5000/tagGame/tagGame.java
+++ src/de/Brogamer5000/tagGame/tagGame.java
@@ -1,13 +1,17 @@
 package de.Brogamer5000.tagGame;
 
 import java.io.File;
+import java.util.logging.Logger;
+
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class tagGame extends JavaPlugin implements Listener{	
 	
-	String pluginPraefix = "§5[tagGame] §f";
-	CommandKit commandExecutor;
+	public static CommandKit commandExecutor;
+	public static tagGame instance;
+	public static Logger logger;
+	public static String prefix;
 	
 	//im Plugin genutzte Berechtigungen:
 	// taggame.reset 
@@ -22,27 +26,44 @@ public class tagGame extends JavaPlugin implements Listener{
 	@Override
 	public void onEnable() {
 
+		//create information for the logger
+		instance = this;
+		prefix = "§5[tagGame] §f";
+		logger = getLogger();
+		
+		//create folder for the stored Information
 		String pluginFolder = this.getDataFolder().getAbsolutePath();
-		(new File(pluginFolder)).mkdir();
+		new File(pluginFolder).mkdir();
 		
-		commandExecutor =  new CommandKit(pluginFolder, pluginPraefix);
 		
+		//save instance of the command-collection class
+		commandExecutor =  new CommandKit(pluginFolder, prefix);
+		
+		//add the 'taggame' command and set its executor and tabCompleter
 		this.getCommand("taggame").setExecutor(commandExecutor);
 		this.getCommand("taggame").setTabCompleter(new TabComplete());
 		
 		
-		//events
-		
+		//create the eventhandler
 		new CustomEventHandler(this);
+		
 		
 		this.getLogger().info("Taggame-Plugin successfully enabled");
 		
-		
 	}
+	
+	
+	
+	
 	
 	@Override
 	public void onDisable() {
+		
+		//save everything
+		commandExecutor.taggedPlayer.save();
+		
 		this.getLogger().info("Taggame-Plugin successfully disabled");
+	
 	}
 
 	
